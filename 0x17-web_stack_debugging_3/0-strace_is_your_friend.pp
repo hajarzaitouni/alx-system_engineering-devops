d1c3b908f4627908a7c7710056b504b4b7d3212a
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-03-07 00:13:07 +0000

    fix apache error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix the Apache why is returning a 500 error using strace
+
+exec { 'fix-error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
